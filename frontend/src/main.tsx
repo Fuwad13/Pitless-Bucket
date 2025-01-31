418d22c9e7f7644188146c50998d33c0ecba5285
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-01-31 18:23:35 +0600

    merge conflict resolved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Login from "./assets/pages/Login";
 import Root from "./assets/routes/root";
+import Home from "./assets/pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <Root />,
-    element: <Login />,
+    element: <Root />,
+    // element: <Login />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "/login",
         element: <Login />,
