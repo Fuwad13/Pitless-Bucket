a73ff0f13d550bba82bc69bed3ceeb2d3e74911c
Fuwad13 <princefuwad@gmail.com>
2025-02-14 03:34:57 +0600

    testing Abstract Factory

--- backend/storage_provider/factory.py
+++ backend/storage_provider/factory.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-from storage_provider.google_drive.provider import GoogleDriveProvider
-from storage_provider.onedrive.provider import OneDriveProvider
-from storage_provider.dropbox.provider import DropBoxProvider
+from backend.storage_provider.google_drive.provider import GoogleDriveProvider
+from backend.storage_provider.onedrive.provider import OneDriveProvider
+from backend.storage_provider.dropbox.provider import DropBoxProvider
 
 
 class StorageProviderFactory(ABC):
@@ -23,3 +23,17 @@ class OneDriveProviderFactory(StorageProviderFactory):
 class DropBoxProviderFactory(StorageProviderFactory):
     def create_provider(self):
         return DropBoxProvider()
+
+
+FACTORY_REGISTRY = {
+    "google_drive": GoogleDriveProviderFactory(),
+    "onedrive": OneDriveProviderFactory(),
+    "dropbox": DropBoxProviderFactory(),
+}
+
+
+def get_provider_factory(provider_name: str):
+    provider_factory = FACTORY_REGISTRY.get(provider_name)
+    if not provider_factory:
+        raise ValueError(f"Invalid provider name: {provider_name}")
+    return provider_factory
