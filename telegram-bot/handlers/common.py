60cb6424464ff0e6b0e835c55dd375497698f50d
Raiyan27 <abdullahalraiyan4@gmail.com>
2025-03-13 13:16:55 +0600

    add: common tg-bot routes

---
+++ telegram-bot/handlers/common.py
@@ -0,0 +1,23 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer('Welcome to PitLess Bucket!')
+
+@router.message(Command("help"))
+async def cmd_help(message: Message):
+    await message.answer("""
+        Available Commands:
+        /start - Show welcome message
+        /link - Link to your PitLess Bucket Account
+        /whoami - Check the current session
+        /logout - Log out of the current session
+        /files - List all available files
+        /upload - Upload a new file
+        /download <filename> - Download a file by name
+        /help - Show this help message
+    """)
