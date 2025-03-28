9fbc2566ca270d12843c853edf496b81dc8642bb
Fuwad Hasan <princefuwad@gmail.com>
2025-03-29 03:54:20 +0600

    Refactor telegram-bot structure

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -3,13 +3,13 @@ import logging
 
 from handlers.echo import echo_router
 from handlers.start import start_router
-# from handlers.pitless_bucket import pb_router
-from handlers.storageStatus import  storage_status_router
+from handlers.pitless_bucket.list_files import list_files_router
+from handlers.pitless_bucket.storage_status import  storage_status_router
 from handlers.common import router as common_router
-from handlers.upload import upload_router
-from handlers.download import download_router
-from handlers.unlink import logout_router
-from handlers.whoami import whoami_router
+from handlers.pitless_bucket.upload import upload_router
+from handlers.pitless_bucket.download import download_router
+from handlers.pitless_bucket.unlink import logout_router
+from handlers.pitless_bucket.whoami import whoami_router
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -27,7 +27,7 @@ async def main() -> None:
         common_router,
         upload_router,
         download_router,
-        # pb_router,
+        list_files_router,
         logout_router,
         whoami_router,
     )
